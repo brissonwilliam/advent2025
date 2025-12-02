158d9874a4f0c0d3b511084e629692218e10bec5
brissonwilliam <brissonwilliam1@hotmail.com>
2025-12-02 01:15:38 -0500

    day1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -13,7 +13,7 @@ R14
 L82
 ";
 
-static VERBOSE: bool = false;
+static VERBOSE: bool = true;
 
 fn main() {
     println!("*******************");
@@ -101,4 +101,93 @@ fn part_one() {
     println!("Answer is: {}", czero);
 }
 
-fn part_two() {}
+fn part_two() {
+    println!("-------------------");
+    println!("PART 2");
+    println!("-------------------");
+
+    // Read the input
+    let mut filename = "input.txt";
+    let mut args = std::env::args();
+    if args.any(|a| return a == "--test") {
+        println!("using test.txt");
+        filename = "test.txt";
+    }
+    let mut f = fs::File::open(filename).unwrap(); // #yolo unwrap() (cloudflare level security right here)
+    let mut input = String::new();
+    let _ = f.read_to_string(&mut input);
+
+    // evaluate time for funz. Ignore file io
+    let start_time = std::time::Instant::now();
+
+    let mut pos: i16 = 50;
+    let mut c: u16 = 0;
+    let mut czero: i16 = 0;
+    for l in input.lines() {
+        // omit empty lines
+        if l == "" {
+            println!("ignoring empty line at {}", c);
+            continue;
+        }
+        if l.len() < 2 {
+            panic!("invalid line input {}", l);
+        }
+        let first_char = l.chars().nth(0).unwrap();
+        let offset_str: String = l.chars().skip(1).collect();
+        let offset: i16 = offset_str.parse().unwrap();
+
+        let sign: i16;
+        if first_char == 'L' || first_char == 'l' {
+            sign = -1
+        } else if first_char == 'R' || first_char == 'r' {
+            sign = 1
+        } else {
+            panic!("invalid line input {}", l);
+        }
+
+        // Same thing as part 1
+        // but now we compute each time we go through 0 (even if not stopping)
+        // Do so by computing full cycles with integer division
+        // then like before, treat the remainder (whilst considering passes to zero)
+        if offset > 99 {
+            println!("  big offset, adding {}", offset / 100);
+            czero += offset / 100;
+        }
+        let remainder = offset % 100;
+
+        // Here we know we can only go pass 0 one time or zero times
+        let newpos = pos + (sign * remainder);
+        if newpos > 99 {
+            // doesn't count if last pos was 0, we did not cross it for real
+            if pos != 0 {
+                czero += 1;
+            }
+            pos = newpos - 100;
+        } else if newpos < 0 {
+            if pos != 0 {
+                czero += 1;
+            }
+            pos = newpos + 100;
+        } else {
+            if newpos == 0 && pos != 0 {
+                czero += 1
+            }
+            pos = newpos;
+        }
+        if VERBOSE {
+            println!(
+                "instruction {} | {} | pos: {} | total: {}",
+                c, l, pos, czero
+            );
+        }
+
+        c += 1;
+    }
+
+    println!(
+        "finished processing {} lines in {}us (without file io)",
+        c,
+        start_time.elapsed().as_micros()
+    );
+    println!("Answer is: {}", czero);
+}
