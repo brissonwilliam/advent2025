e9f9b64ae348ba840c0eba50025e6329e0d8e575
brissonwilliam <brissonwilliam1@hotmail.com>
2025-12-09 16:03:07 -0500

    day7 part 2, I hate i32 quietly overflowing :')

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 mod data;
 
 static TESTINPUT: &str = "
@@ -19,6 +21,15 @@ static TESTINPUT: &str = "
 ...............
 ";
 
+static TESTINPUT2: &str = "
+.......S.......
+...............
+.......^.......
+...............
+......^........
+...............
+";
+
 static VERBOSE: bool = false;
 
 fn main() {
@@ -33,10 +44,16 @@ fn main() {
             input = TESTINPUT;
             break;
         }
+        if a == "--test2" {
+            println!("using test data 2");
+            input = TESTINPUT2;
+            break;
+        }
     }
     input = input.trim();
 
     part1(String::from(input));
+    part2(input);
 }
 
 fn part1(mut input: String) {
@@ -69,7 +86,7 @@ fn part1(mut input: String) {
                 continue;
             }
 
-            if bytes[pos] == b'^' {
+            if c == '^' {
                 if VERBOSE {
                     println!("beam ^ detected at {} ({}, {})", pos, x, y);
                 }
@@ -105,6 +122,101 @@ fn part1(mut input: String) {
 
     let elapsed = start_time.elapsed();
     println!("------------ PART 1 -----------------");
+    println!("Anwser is \n{}\n", answer);
+    println!("processing time {}us", elapsed.as_micros());
+    println!("-------------------------------------");
+}
+
+fn explore_outcomes(
+    mut input_state: String,
+    x: i32,
+    y: usize,
+    width_real: usize,
+    height: usize,
+    memory: &mut HashMap<usize, i64>,
+) -> i64 {
+    let bytes = unsafe { input_state.as_bytes_mut() };
+
+    let mut outcomes = 0;
+    for newy in y..height {
+        // if we already visited the position, ignore.
+        // It will be accounted for from another branch that already visitted this
+        let pos = newy * width_real + x as usize;
+
+        if bytes[pos] == b'^' {
+            if let Some(&cached_outcomes) = memory.get(&pos) {
+                outcomes = cached_outcomes;
+                break;
+            }
+
+            // Compute left side paths
+            // Ignore if it's a ^ neighbor or out of bounds
+            if x as i32 - 1 >= 0 && bytes[pos - 1] == b'.' {
+                let strcpy = String::from_utf8(bytes.to_vec()).unwrap();
+                let outcomes_left =
+                    explore_outcomes(strcpy, x - 1, newy, width_real, height, memory);
+                if outcomes_left == 0 {
+                    // dead end branch, no more splitters
+                    outcomes += 1
+                } else {
+                    outcomes += outcomes_left
+                }
+            }
+
+            // Compute right side paths
+            // Ignore if it's a ^ neighbor or out of bounds
+            if x as usize + 1 < (width_real - 1) && bytes[pos + 1] == b'.' {
+                let strcpy = String::from_utf8(bytes.to_vec()).unwrap();
+                let outcomes_right =
+                    explore_outcomes(strcpy, x + 1, newy, width_real, height, memory);
+
+                if outcomes_right == 0 {
+                    // there will be no next values, only 2 dead end branches
+                    outcomes += 1;
+                } else {
+                    outcomes += outcomes_right
+                }
+            }
+
+            memory.insert(pos, outcomes);
+            break;
+        }
+        bytes[pos] = b'|';
+    }
+
+    if VERBOSE {
+        println!(
+            "finished processing outcomes={},x={},y={}\n{}",
+            outcomes, x, y, input_state,
+        );
+    }
+
+    return outcomes;
+}
+
+fn part2(input: &str) {
+    let start_time = std::time::Instant::now();
+
+    let width = input.lines().next().unwrap().len();
+    let width_real = width + 1;
+    let height = input.lines().count();
+
+    let mut memory: HashMap<usize, i64> = HashMap::new();
+
+    let mut cpy = String::new();
+    cpy.push_str(input);
+
+    let bytes = unsafe { cpy.as_bytes_mut() };
+    let x = width / 2;
+    bytes[width_real + x] = b'|';
+
+    let strcpy = String::from_utf8(bytes.to_vec()).unwrap();
+    let answer = explore_outcomes(strcpy, x as i32, 1, width_real, height, &mut memory);
+
+    println!("visited {}", memory.len());
+
+    let elapsed = start_time.elapsed();
+    println!("------------ PART 2 -----------------");
     println!("Anwser is \n{}\n", answer);
     println!("processing time {}us", elapsed.as_micros());
     println!("-------------------------------------");
